2b1855ef727fb7be00593a066e5e7f31f71e585b
Alfred Chao <94402622+alfredchao17@users.noreply.github.com>
2021-12-15 15:43:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,62 +4,150 @@ console.log("script is loading");
 const descriptionText = document.querySelector(".adventure-description");
 const optionButtons = document.getElementsByClassName("button");
 
+// Id node where the game starts
+let startIndex = 0;
+
 // function that starts the game
-function startAdventure() {
+function startAdventure(textAdventureIndex) {
   console.log("Adventure has started");
-  showAdventureOptions(1);
+  showAdventureOptions(textAdventureIndex);
 }
 
 // create a function that shows the description
 function showAdventureOptions(textAdventureID) {
-  const newDescriptionText = textAdventure.find(gameNode => gameNode.id === textAdventureID);
+  // check fisrt
+
+  const newDescriptionText = textAdventure.find(
+    (gameNode) => gameNode.id === textAdventureID
+  );
+  if (!newDescriptionText.options) {
+    // hide button
+    document.getElementById("first_btn").style.display = "none";
+
+    // show description
+    descriptionText.innerHTML = newDescriptionText.message;
+
+    document.getElementById("second_btn").innerHTML = "Play Again";
+    return;
+  }
+
   descriptionText.innerText = newDescriptionText.description;
   // iterate through the buttons and insert the option text in each button
-  for(let i = 0; i < newDescriptionText.options.length; i++) {
+  for (let i = 0; i < newDescriptionText.options.length; i++) {
     //console.log(newDescriptionText.options[i].buttontext);
     optionButtons[i].innerText = newDescriptionText.options[i].buttontext;
   }
 }
 
 // function that tracks options and passes the id to the showAdventureOptions function
+function buttonOptions() {
+  const adventureIndex = textAdventure.find(
+    (gameNode) => gameNode.id === startIndex
+  );
 
+  for (let i = 0; i < optionButtons.length; i++) {
+    optionButtons.addEventListener("click", function () {});
+  }
+}
+
+function onPlay() {
+  let ids = textAdventure.map((item) => {
+    return item.id;
+  });
+
+  if (startIndex > ids.length - 1) {
+    startIndex = 0;
+  }
+
+  // show
+  showAdventureOptions(ids[startIndex]);
+}
+
+function onNextOption(event) {
+  event.preventDefault();
+
+  startIndex++;
+
+  onPlay();
+}
+
+function onPlayAgain(event) {
+  event.preventDefault();
+
+  // show button
+  document.getElementById("first_btn").style.display = "inline";
+
+  startIndex = 0;
+
+  onPlay();
+}
 
 // optional: function that shows end of the game
-
 
 // create and object that holds the text for our game and options and also tracks state
 const textAdventure = [
   {
     id: 1,
-    description: 'This is the adventure of Rio, who got lost in the dark when seaching for the asker. Rio needs your help to find her way out. Choose one of the options below',
+    description:
+      "This is the adventure of Rio, who got lost in the dark when searching for the asker. Rio needs your help to find her way out. Choose one of the options below",
     options: [
       {
         buttontext: "Head back to where you came from",
         setState: { headback: true },
-        nexttext: 2
+        nexttext: 2,
       },
       {
         buttontext: "Look up at the sky and head towards the sun",
-        nextText: 2       
-      }
-    ]
+        message: console.log("Try again"),
+      },
+    ],
   },
   {
     id: 2,
-    description: 'great, Rio was able to find her way back, but now she reliazes that its about to rain, what should Rio do now?',
+    description:
+      "great, Rio was able to find her way back, but now she reliazes that its about to rain, what should Juan do now?",
     options: [
       {
         buttontext: "Look for shelter",
         setState: { headback: true },
-        nexttext: 3
+        nexttext: 3,
       },
       {
         buttontext: "Continue heading back, and risk getting lost again",
-        nextText: 2       
-      }
-    ]
+        message: console.log("Try again"),
+      },
+    ],
   },
-  
+  {
+    id: 3,
+    description:
+      "Rio is safe in the shelter. She will wait until it stops raining. What should Rio do",
+    options: [
+      {
+        buttontext: "Leave the shelter as soon as it stops raining",
+        setState: { headback: true },
+      },
+      {
+        buttontext: "Wait until the sun comes out again and continue walking",
+        setState: { daylight: true },
+        nexttext: 4,
+      },
+    ],
+  },
+  {
+    id: 4,
+    // message: console.log('Thank you for helping Rio!')
+    message: "Thank you for helping Rio!",
+  },
 ];
 
-startAdventure();
+// get first index id
+// let ids = textAdventure.map(item => {
+//   return item.id;
+// });
+
+// let firstId = ids[startIndex];
+
+// startAdventure(firstId);
+
+onPlay();
